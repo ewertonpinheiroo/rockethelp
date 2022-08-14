5024afd53746c45f1658f571cb156397afd29e2a
unknown <ewertonpinheiro@gmail.com>
2022-08-14 02:01:52 -0300

    primeiro commmit

---
+++ App.tsx
@@ -0,0 +1,30 @@
+import { NativeBaseProvider,StatusBar } from "native-base";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
+import { THEME } from "./src/styles/theme";
+
+
+import { Routes } from "./src/routes";
+import { SignIn } from "./src/screens/Signin";
+import { Loading } from "./src/components/Loading";
+import { Home } from "./src/screens/Home";
+import { Register } from "./src/screens/Register";
+
+
+export default function App() {
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold })
+
+  return (
+  <NativeBaseProvider theme={THEME}>
+ <StatusBar
+ barStyle="light-content"
+ backgroundColor="transparent"
+ translucent
+ 
+ />
+ {fontsLoaded ? <Routes/> : <Loading/>}
+  </NativeBaseProvider>
+ 
+  );
+}
+
+
---
+++ src/components/input.tsx
@@ -0,0 +1,20 @@
+import { Input as NativeBaseInput, IInputProps } from 'native-base';
+
+export function Input({...rest}: IInputProps) {
+  return (
+    <NativeBaseInput
+    bg="gray.700"
+    h={14}
+    size="mb"
+    fontFamily="body"
+    color="white"
+    placeholderTextColor="gray.300"
+    _focus={{
+        borderWidth: 1,
+        borderColor: 'green.500',
+        bg: "gray.700"
+    }}
+    {...rest}
+    />
+  );
+}
